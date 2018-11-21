cef04cba5ef57a36bc7f7be4cbba787f1a81a3ef
Jhosep Bazan <jhosepv13@hotmail.com>
2018-11-21 16:43:04 -0500

    Second Commit :)

---
+++ PokemonGO/PokedexViewController.swift
@@ -0,0 +1,63 @@
+//
+//  PokedexViewController.swift
+//  PokemonGO
+//
+//  Created by Jhosep Bazan on 21/11/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var pokemonsAtrapados:[Pokemon] = []
+    var pokemonsNoAtrapados:[Pokemon] = []
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        pokemonsAtrapados = obtenerPokemonsAtrapados()
+        pokemonsNoAtrapados = obtenerPokemonsNoAtrapados()
+    
+    }
+    
+    @IBAction func mapTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return pokemonsAtrapados.count
+        }else{
+            return pokemonsNoAtrapados.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pokemon : Pokemon
+        if indexPath.section == 0{
+            pokemon = pokemonsAtrapados[indexPath.row]
+        }else{
+            pokemon = pokemonsNoAtrapados[indexPath.row]
+        }
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.nombre
+        cell.imageView?.image = UIImage(named: pokemon.imagenNombre!)
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "Atrapados"
+        }else{
+            return "No atrapdos"
+        }
+    }
+    
+}
--- PokemonGO/ViewController.swift
+++ PokemonGO/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    var ubicacion = CLLocationManager()
+    var contActualizaciones = 0
+    var pokemons:[Pokemon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ubicacion.delegate = self
+        pokemons = obtenerPokemons()
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapView.delegate = self
+            mapView.showsUserLocation = true
+            ubicacion.startUpdatingLocation()
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: {(timer) in
+                if let coord = self.ubicacion.location?.coordinate{
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = coord
+                    let randomLat = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    let randomLon = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    pin.coordinate.longitude += randomLon
+                    pin.coordinate.latitude += randomLat
+                    self.mapView.addAnnotation(pin)
+                }
+            })
+        }
+        else{
+            ubicacion.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if (contActualizaciones < 1){
+            let region = MKCoordinateRegionMakeWithDistance(ubicacion.location!.coordinate, 1000, 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        }else{
+            ubicacion.stopUpdatingLocation()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centraTapped(_ sender: Any) {
+        if let coord = ubicacion.location?.coordinate{
+            let region = MKCoordinateRegionMakeWithDistance(coord, 1000, 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        }
     }
-
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            let pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            pinView.image = UIImage(named:"player")
+            
+            var frame = pinView.frame
+            frame.size.height = 50
+            frame.size.width = 50
+            pinView.frame = frame
+            
+            return pinView
+        }
+        
+        let pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pinView.image = UIImage(named: "mew")
+        
+        var frame = pinView.frame
+        frame.size.height = 50
+        frame.size.width = 50
+        pinView.frame = frame
+        
+        return pinView
+    }
 
 }
 
